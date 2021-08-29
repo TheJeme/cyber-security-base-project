8a8deec034e351e706706a1632e1de76f833ef4a
TheJeme <eemelijoonatan@gmail.com>
2021-08-29 17:20:06 +0300

    project done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3003";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState(false);
   const [PIN, setPIN] = useState("");
+  useEffect(() => {
+    if (isLoggedIn) {
+      axios
+        .get("/api/users")
+        .then((res) => {
+          res.data.forEach((user) => {
+            if (user.username === username) {
+              setPIN(user.pin);
+            }
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [isLoggedIn]);
   return (
     <div>
       {isLoggedIn ? (
         <h1>Your PIN is: {PIN}</h1>
       ) : (
-        <LoginForm setIsLoggedIn={setIsLoggedIn} setPIN={setPIN} />
+        <LoginForm
+          setIsLoggedIn={setIsLoggedIn}
+          setPIN={setPIN}
+          setUsername={setUsername}
+        />
       )}
     </div>
   );
@@ -28,7 +49,8 @@ const LoginForm = (props) => {
         password: `${password}`,
       })
       .then((res) => {
-        props.setPIN(res.data.pin);
+        //props.setPIN(res.data.pin);
+        props.setUsername(res.data.username);
         props.setIsLoggedIn(true);
       })
       .catch((err) => {
